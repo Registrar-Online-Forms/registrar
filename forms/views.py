578f6731d2cd65ee339f1bf0d7148855dc4e8d69
joespeaker <joebieker2486@outlook.com>
2023-01-24 02:28:38 -0600

    Created a working index page that displays a simple welcome screen.

--- forms/views.py
+++ forms/views.py
@@ -6,4 +6,18 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the forms index.")
+    # user is an object variable with firstname and lastname attributes
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    # context is a dictionary variable with key 'user' and value user
+    # this references the user object variable
+    # and passes it to the web page. Variables in the web page are referenced by {{objectname.attribute}}
+    one = {'formType': 'Change Adivsor', 'submission' : '1/23/2023', 'status': 'Pending'}
+    two = {'formType': 'Drop/Add Class', 'submission' : '1/2/2022', 'status': 'Awaiting Registrar Approval'}
+    three = {'formType': 'Drop/Add Class', 'submission' : '12/23/2022', 'status': 'Awaiting Advisor Approval'}
+    four = {'formType': 'Drop/Add Major', 'submission' : '10/10/2022', 'status': 'Complete'}
+    recents = {'one': one, 'two': two, 'three': three, 'four': four}
+    context= {
+        'user': user,
+        'recents': recents
+        }
+    return render(request, 'index.html', context)
