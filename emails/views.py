0d0a66269265c6e88de16e97deb2f1876453ed4f
TheColleenDoran <119897972+TheColleenDoran@users.noreply.github.com>
2023-01-29 16:21:38 -0600

    Emails are being sent!
    Figured out how to send emails.
    Had to switch to my personal account.
    Also made it a method able to be called with the argument of the recipient.
    Should be able to be changed to take an array of recipients instead of just one.

--- emails/views.py
+++ emails/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 
 # Create your views here.
 from django.http import HttpResponse
 
+# def index(request):
+#     send_mail('IT WORKS!',
+#         'THANK THE LORD!',
+#         'dora3649@ravens.benedictine.edu',
+#         ['colleendoran314@gmail.com'],
+#         fail_silently=False)
+#     return HttpResponse("Welcome to the email sender! Your email has been sent.")
 def index(request):
-    send_mail('IT WORKS!',
-        'THANK THE LORD!',
-        'dora3649@ravens.benedictine.edu',
-        ['colleendoran314@gmail.com'],
-        fail_silently=False)
+    sendEmail('colleendoran314@gmail.com')
     return HttpResponse("Welcome to the email sender! Your email has been sent.")
+
+def sendEmail(to):
+    msg = EmailMessage('IT WORKS!',
+                        'You can read this!',
+                        to = [to])
+    msg.send()
