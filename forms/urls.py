1d4e15ac5ea424edf997032dca448ab4afee45f5
joespeaker <joebieker2486@outlook.com>
2023-03-20 09:09:45 -0500

    update to forms

--- forms/urls.py
+++ forms/urls.py
@@ -7,11 +7,34 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('addDropClass', views.addDropClass, name='addDropClass'),
     path('registrar', views.registrar, name='registrar'),
     path('professor', views.professor, name='professor'),
     path('new', views.newForm, name='newForm'),
     path('help', views.help, name='help'),
-    path('history', views.history, name='history')
+    path('history', views.history, name='history'),
+    path('addDropClass', views.addDropClass, name='addDropClass'),
+    path('declareMajor', views.declareMajor, name='declareMajor'),
+    path('declareMinor', views.declareMinor, name='declareMinor'),
+    path('dropMajor', views.dropMajor, name='dropMajor'),
+    path('dropMinor', views.dropMinor, name='dropMinor'),
+    path('auditToCredit', views.auditToCredit, name='auditToCredit'),
+    path('creditToAudit', views.creditToAudit, name='creditToAudit'),
+    path('overload', views.overload, name='overload'),
+    path('requestPassNoPass', views.requestPassNoPass, name='requestPassNoPass'),
+    path('internshipContract', views.internshipContract, name='internshipContract'),
+    path('changeAdvisor', views.changeAdvisor, name='changeAdvisor')
+
 ]
 
+#list of forms
+#addDropClass
+#declareMajor
+#declareMinor
+#dropMajor
+#dropMinor
+#auditToCredit
+#creditToAudit
+#overload
+#requestPassNoPass
+#internshipContract
+#changeAdvisor
--- forms/views.py
+++ forms/views.py
@@ -5,6 +5,7 @@ from .forms import MyForm
 # Create your views here.
 
 from django.http import HttpResponse
+import datetime
 
 
 def index(request):
@@ -24,12 +25,11 @@ def index(request):
         }
     return render(request, 'index.html', context)
 
-def addDropClass(request):
-    user = {'firstname': 'John', 'lastname': 'Doe'}
-    context = {
-        'user': user
-        }
-    return render(request, 'addDropClass.html',context)
+def registrar(request):
+    return render(request, 'registrar.html')
+
+def professor(request):
+    return render(request, 'professor.html')
 
 def newForm(request):
     user = {'firstname': 'John'} # This is only needed to show "Welcome [Student]" - Ben
@@ -55,3 +55,123 @@ def history(request):
         'recents': recents
         }
     return render(request, 'history.html', context)
+
+#all form urls
+def addDropClass(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now()}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'addDropClass.html',context)
+
+def declareMajor(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'declareMajor.html',context)
+
+def declareMinor(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'declareMinor.html',context)
+
+def dropMajor(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'dropMajor.html',context)
+
+def dropMinor(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'dropMinor.html',context)
+
+def auditToCredit(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'auditToCredit.html',context)
+
+def creditToAudit(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'creditToAudit.html',context)
+
+def overload(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'overload.html',context)
+
+def changeFinal(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'changeFinal.html',context)
+
+def requestPassNoPass(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'requestPassNoPass.html',context)
+
+def internshipContract(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'internshipContract.html',context)
+
+def changeAdvisor(request):
+    user = {'firstname': 'John', 'lastname': 'Doe'}
+    date = {'date': datetime.datetime.now(),
+            'year': datetime.datetime.now().year,}
+    context = {
+        'user': user,
+        'date': date
+        }
+    return render(request, 'changeAdvisor.html',context)
