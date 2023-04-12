08659fdcc22a4123e7e63059a9a62be8f0d5bef2
daniel9wee <wee2379@ravens.benedictine.edu>
2023-04-12 12:34:04 -0500

    Added comments to database model

--- forms/models.py
+++ forms/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+# Student class stores the basic information of a student
 class Student(models.Model):
     id = models.CharField(primary_key=True, max_length=10)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
 
-
+# Course class stores the information about a course
 class Course(models.Model):
     course_number = models.CharField(max_length=10)
     instructor_name = models.CharField(max_length=50)
     instructor_email = models.EmailField()
 
+# Form class is the base class for all other forms and stores the information common to all forms
 class Form(models.Model):
     PENDING = 'P'
     APPROVED = 'A'
@@ -29,24 +30,21 @@ class Form(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+# AddDropClassForm class stores the information related to a student adding or dropping a course
 class AddDropClassForm(models.Model):
     form = models.OneToOneField(Form, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    student_signature = models.ImageField(upload_to='signatures/')
-    instructor_signature = models.ImageField(upload_to='signatures/')
     additional_approvers_emails = models.TextField(blank=True, null=True)
 
-
+# AuditToCreditForm class stores the information related to a student converting an audit course to a credit course
 class AuditToCreditForm(models.Model):
     form = models.OneToOneField(Form, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     advisor_name = models.CharField(max_length=50)
     advisor_email = models.EmailField()
-    student_signature = models.ImageField(upload_to='signatures/')
-    instructor_signature = models.ImageField(upload_to='signatures/')
-    advisor_signature = models.ImageField(upload_to='signatures/')
     additional_approvers_emails = models.TextField(blank=True, null=True)
 
+# CreditToAuditForm class stores the information related to a student converting a credit course to an audit course
 class CreditToAuditForm(models.Model):
     form = models.OneToOneField(Form, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -54,34 +52,28 @@ class CreditToAuditForm(models.Model):
     advisor_email = models.EmailField()
     instructor_name = models.CharField(max_length=50)
     instructor_email = models.EmailField()
-    advisor_signature = models.ImageField(upload_to='signatures/')
-    instructor_signature = models.ImageField(upload_to='signatures/')
     department_chair_name = models.CharField(max_length=50)
     department_chair_email = models.EmailField()
-    department_chair_signature = models.ImageField(upload_to='signatures/')
     additional_approvers_emails = models.TextField(blank=True, null=True)
 
+# DeclareMajorForm class stores the information related to a student declaring a major
 class DeclareMajorForm(models.Model):
     form = models.OneToOneField(Form, on_delete=models.CASCADE)
-    student_signature = models.ImageField(upload_to='signatures/')
     student_catalogue_year = models.IntegerField()
     student_anticipted_degree_date = models.DateField()
     student_declared_major = models.BooleanField(default=False)
     department_chair_name = models.CharField(max_length=50)
     department_chair_email = models.EmailField()
-    department_chair_signature = models.ImageField(upload_to='signatures/')
     additional_approvers_emails = models.TextField(blank=True, null=True)
 
-
+# DeclareMinorForm class stores the information related to a student declaring a minor
 class DeclareMinorForm(models.Model):
     form = models.OneToOneField(Form, on_delete=models.CASCADE)
-    student_signature = models.ImageField(upload_to='signatures/')
     student_catalogue_year = models.IntegerField()
     student_anticipted_degree_date = models.DateField()
     student_declared_minor = models.BooleanField(default=False)
     department_chair_name = models.CharField(max_length=50)
     department_chair_email = models.EmailField()
-    department_chair_signature = models.ImageField(upload_to='signatures/')
     additional_approvers_emails = models.TextField(blank=True, null=True)
 
     
