660cd1869379f3c6395b8e467e6c2551310555ac
TheColleenDoran <119897972+TheColleenDoran@users.noreply.github.com>
2023-04-22 12:24:37 -0500

    Revert "Revert "Revert "commit from server"""
    This reverts commit 8efdb985d085631777360caa359d0818d58cf177.

--- registrar/settings/base.py
+++ registrar/settings/base.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_auth_adfs.middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'registrar.urls'
